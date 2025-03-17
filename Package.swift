40a1f3ee135b54e0100e27821eb19006423917dc
Wadÿe <midou.khebbeb@icloud.com>
2025-03-17 08:22:35 +0100

    now called CestrumCore

--- Package.swift
+++ Package.swift
@@ -4,21 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "Cestrum",
+    name: "CestrumCore",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Cestrum",
-            targets: ["Cestrum"]),
+            name: "CestrumCore",
+            targets: ["CestrumCore"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Cestrum"),
+            name: "CestrumCore"),
         .testTarget(
-            name: "CestrumTests",
-            dependencies: ["Cestrum"]
+            name: "CestrumCoreTests",
+            dependencies: ["CestrumCore"]
         ),
     ]
 )
