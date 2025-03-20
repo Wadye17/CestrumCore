bcfe4c18aafeb49fb5a715480fa303b146057cc0
Wadÿe <midou.khebbeb@icloud.com>
2025-03-20 12:50:33 +0100

    Update Graph.swift

--- Sources/Cestrum/Graph/Graph.swift
+++ Sources/Cestrum/Graph/Graph.swift
@@ -135,7 +135,7 @@ extension DependencyGraph: CustomStringConvertible {
     public var description: String {
         """
         graph \(self.namespace) {
-            nodes {\(self.nodes.map(\.description).sorted().joined(separator: ", "))}
+            deployments {\(self.nodes.map(\.description).sorted().joined(separator: ", "))}
             dependencies {
             \t\(self.arcs.map(\.description).sorted().joined(separator: "\n\t\t"))
             }
